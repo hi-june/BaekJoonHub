9d844a34fbeae84904be857cc180a83f009d8571
hi-june <98803599+hi-june@users.noreply.github.com>
2023-05-26 23:53:27 +0900

    [level 1] Title: K번째수, Time: 0.36 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -6,19 +6,15 @@ class Solution {
         int n = commands.length;
         int[] answer = new int[n];
         
-        List<Integer> nums = Arrays.stream(array)
-            .boxed()
-            .collect(Collectors.toList());
-        
         int idx = 0;
         for (int[] c : commands) {
             int i = c[0];
             int j = c[1];
             int k = c[2];
             
-            List<Integer> subNums = new ArrayList<>(nums.subList(i - 1, j));
-            Collections.sort(subNums);
-            answer[idx++] = subNums.get(k - 1);
+            int[] subArray = Arrays.copyOfRange(array, i - 1, j);
+            Arrays.sort(subArray);
+            answer[idx++] = subArray[k - 1];
         }
         
         return answer;
