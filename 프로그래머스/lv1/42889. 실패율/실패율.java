bb4d3732c7d437cdb9fc8133ba7321d03498afd3
hi-june <98803599+hi-june@users.noreply.github.com>
2023-10-05 11:32:05 +0900

    [level 1] Title: 실패율, Time: 5.51 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        Map<Integer, Double> ranking = new HashMap<>();
+        
+        for (int i = 1; i <= N; i++) {
+            int finalI = i;
+            
+            long n = Arrays.stream(stages)
+                .filter(s -> s == finalI)
+                .count();
+            long m = Arrays.stream(stages)
+                .filter(s -> s >= finalI)
+                .count();
+            
+            double rank = (m == 0) ? 0 : (double)n / m;
+            ranking.put(i, rank);
+        }
+        
+        return ranking.keySet().stream()
+                .sorted((k1, k2) -> {
+                    if (ranking.get(k2).compareTo(ranking.get(k1)) == 0) return k1.compareTo(k2);
+                    return ranking.get(k2).compareTo(ranking.get(k1));})
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
