0e76b7cd8dc5fa3ece59005c05dbc2b0e2c68074
hi-june <98803599+hi-june@users.noreply.github.com>
2024-01-16 14:26:20 +0900

    [level Lv.1] Title: 체육복, Time: 1.11 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/42862. 체육복/체육복.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> reserveSet = Arrays.stream(reserve)
+            .boxed()
+            .collect(Collectors.toSet());
+
+        for (int i : lost) {
+            if (reserveSet.contains(i)) {
+                reserveSet.remove(i);
+            } else {
+                lostSet.add(i);
+            }
+        }
+
+        for (Integer i : reserveSet) {
+            if (lostSet.contains(i - 1)) {
+                lostSet.remove(i - 1);
+            } else if (lostSet.contains(i + 1)) {
+                lostSet.remove(i + 1);
+            }
+        }
+
+        return n - lostSet.size();
+    }
+}
