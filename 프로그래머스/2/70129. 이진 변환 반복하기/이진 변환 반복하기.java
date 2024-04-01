5333c518f58233da472c29c92a3667e3a7e73bcb
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-04-02 00:44:22 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.71 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,42 +1,29 @@
-import java.util.Stack;
-
 class Solution {
-    private static int binCount = 0;
-    private static int zeroCount = 0;
-    
     public int[] solution(String s) {        
+        int binCount = 0;
+        int zeroCount = 0;
+        
         while (!s.equals("1")) {
             // 1. 0 변환
-            int beforeLen = s.length();
-            s = s.replace("0", "");
-            
-            int c = s.length();
-            zeroCount += beforeLen - c;
+            int zeros = getZeroCount(s);
+            int c = s.length() - zeros;
+            zeroCount += zeros;
             
             // 2. 2진법 변환
-            s = getBinary(c);
+            s = Integer.toString(c, 2);
             binCount++;
         }
 
         return new int[] {binCount, zeroCount}; // 이진 변환의 횟수, 변환 과정에서 제거된 모든 0의 개수
     }
     
-    private String getBinary(int c) {
-        StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
+    private int getZeroCount(String s) {
+        int count = 0;
         
-        while (c / 2 != 0) {
-            int remain = c % 2;            
-            stack.push(remain);
-            
-            c = c / 2;
-        }
-        stack.push(c);
-        
-        while (!stack.isEmpty()) {
-            sb.append(String.valueOf(stack.pop()));
+        for (char c : s.toCharArray()) {
+            if (c == '0') count++;
         }
         
-        return sb.toString();
+        return count;
     }
 }
