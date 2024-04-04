a1d8d7ba459442f56da73ef4e429b2fb5f5a99d9
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-04-04 17:13:17 +0900

    [level 2] Title: 순위 검색, Time: 1167.97 ms, Memory: 242 MB -BaekjoonHub

---
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -0,0 +1,84 @@
+import java.util.*;
+import java.util.function.Consumer;
+
+class Solution {
+    private void forEachKey(int index, String prefix, String[] tokens, Consumer<String> action) {
+        if (index == tokens.length - 1) {
+            action.accept(prefix);
+            return;
+        }
+
+        forEachKey(index + 1, prefix + tokens[index], tokens, action);
+        forEachKey(index + 1, prefix + "-", tokens, action);
+    }
+
+    private Map<String, List<Integer>> buildScoresMap(String[] info) {
+        Map<String, List<Integer>> scoresMap = new HashMap<>();
+
+        for (String s : info) {
+            String[] tokens = s.split(" ");
+            int score = Integer.parseInt(tokens[tokens.length - 1]);
+
+            forEachKey(0, "", tokens, key -> {
+                scoresMap.putIfAbsent(key, new ArrayList<>());
+                scoresMap.get(key).add(score);
+            });
+        }
+
+        for (List<Integer> list : scoresMap.values()) {
+            Collections.sort(list);
+        }
+
+        return scoresMap;
+    }
+
+    private int count(String query, Map<String, List<Integer>> scoresMap) {
+        String[] tokens = query.split(" (and )?");
+
+        String key = String.join("", Arrays.copyOf(tokens, tokens.length - 1));
+
+        if (!scoresMap.containsKey(key)) return 0;
+        List<Integer> scores = scoresMap.get(key);
+
+        int score = Integer.parseInt(tokens[tokens.length - 1]);
+
+        // int targetIdx = Collections.binarySearch(scores, score);
+        // targetIdx = (targetIdx >= 0) ? targetIdx : Math.abs(targetIdx) - 1;
+
+        // return scores.size() - targetIdx;
+        return scores.size() - binarySearch(score, scores);
+    }
+    
+    private int binarySearch(int score, List<Integer> scores) {
+        int start = 0;
+        int end = scores.size() - 1;
+
+        while (end > start) {
+            int mid = (start + end) / 2;
+
+            if (scores.get(mid) >= score) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        if (scores.get(start) < score) {
+            return scores.size();
+        }
+
+        return start;
+    }
+
+    public int[] solution(String[] info, String[] query) {
+        Map<String, List<Integer>> scoreMap = buildScoresMap(info);
+
+        int[] answer = new int[query.length];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = count(query[i], scoreMap);
+        }
+
+        return answer;
+    }
+}
