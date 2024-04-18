6f4336314bb150a0ccdcfe8ba8e55321a53a2317
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-04-18 14:48:19 +0900

    [Gold IV] Title: 부분합, Time: 320 ms, Memory: 25892 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -8,47 +8,49 @@ public class Main {
 
         String[] inputs = br.readLine().split(" ");
         int N = Integer.parseInt(inputs[0]);
-        long S = Long.parseLong(inputs[1]);
+        int S = Integer.parseInt(inputs[1]);
 
-        long[] nums = Arrays.stream(br.readLine().split(" "))
-            .mapToLong(Long::parseLong)
+        inputs = br.readLine().split(" ");
+        int[] nums = Arrays.stream(inputs)
+            .mapToInt(Integer::parseInt)
             .toArray();
 
-        if (Arrays.stream(nums).sum() == S) {
-            bw.write(Integer.toString(nums.length));
-            bw.flush();
-
-            br.close();
-            bw.close();
-            return;
-        }
-
         int start = 0;
-        int end = nums.length - 1;
+        int end = N - 1;
 
         int s = 0;
         int e = s;
-        long total = nums[s];
 
-        while (s < nums.length) {
-            if (total >= S) {
+        int total = Arrays.stream(nums).sum();
+        if (total == S) {
+            bw.write(Integer.toString(N));
+            bw.flush();
+
+            bw.close();
+            br.close();
+            return;
+        }
+
+        int sum = nums[s];
+        while (s <= e) {
+            if (sum >= S) {
                 if (e - s < end - start) {
-                    end = e;
                     start = s;
+                    end = e;
                 }
 
-                total = total - nums[s++];
-            } else if (e < nums.length - 1) {
-                total = total + nums[++e];
+                sum -= nums[s];
+                s++;
+            } else if (e < N - 1) {
+                e++;
+                sum += nums[e];
             } else {
                 break;
             }
         }
 
-        int answer = (end - start == nums.length - 1) ? 0 : end - start + 1;
-
+        int answer = (end - start + 1 == N) ? 0 : end - start + 1;
         bw.write(Integer.toString(answer));
-        bw.flush();
 
         br.close();
         bw.close();
