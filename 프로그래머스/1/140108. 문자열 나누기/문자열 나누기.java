6cc1861225014b2d33bb70b6cad57e68226a89a2
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-04-19 16:23:14 +0900

    [level 1] Title: 문자열 나누기, Time: 0.69 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,31 @@
+import java.util.*;
+// import java.util.stream.*;
+
+class Solution {
+    public int solution(String s) {
+        int strCount = 0;
+        
+        int idx = 0;
+        while (idx < s.length()) {
+            char c = s.charAt(idx);
+            
+            int targetCount = 1;
+            int compCount = 0;
+            
+            while (targetCount != compCount && idx < s.length() - 1) {
+                idx++;
+                
+                char comp = s.charAt(idx);
+                if (c == comp) {
+                    targetCount++;
+                } else {
+                    compCount++;
+                }
+            }
+            strCount++;
+            idx++;
+        }
+        
+        return strCount;
+    }
+}
