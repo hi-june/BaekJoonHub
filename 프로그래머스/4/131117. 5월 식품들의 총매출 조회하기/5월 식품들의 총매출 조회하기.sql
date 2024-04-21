f139a217f21cec66be02d1c378719f099ad3fefc
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-04-21 13:41:21 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,19 +1,25 @@
--- 코드를 입력하세요
+# -- 코드를 입력하세요
+# select P.PRODUCT_ID, P.PRODUCT_NAME, (P.PRICE * O.AMOUNT) as TOTAL_SALES
+# from 
+#     FOOD_PRODUCT as P
+#     inner join (
+#         select PRODUCT_ID, sum(AMOUNT) as AMOUNT, PRODUCE_DATE
+#         from FOOD_ORDER
+#         where DATE_FORMAT(PRODUCE_DATE, '%Y-%m') = '2022-05'
+#         group by PRODUCT_ID
+#     ) as O
+#     on P.PRODUCT_ID = O.PRODUCT_ID
+# order by TOTAL_SALES desc, P.PRODUCT_ID asc;
+
+
 select P.PRODUCT_ID, P.PRODUCT_NAME, (P.PRICE * O.AMOUNT) as TOTAL_SALES
 from 
     FOOD_PRODUCT as P
     inner join (
-        select PRODUCT_ID, sum(AMOUNT) as AMOUNT, PRODUCE_DATE
+        select PRODUCT_ID, sum(AMOUNT) AS AMOUNT, PRODUCE_DATE
         from FOOD_ORDER
         where DATE_FORMAT(PRODUCE_DATE, '%Y-%m') = '2022-05'
         group by PRODUCT_ID
     ) as O
     on P.PRODUCT_ID = O.PRODUCT_ID
 order by TOTAL_SALES desc, P.PRODUCT_ID asc;
-
-
-
-# select PRODUCT_ID, sum(AMOUNT) as AMOUNT, PRODUCE_DATE
-# from FOOD_ORDER
-# where DATE_FORMAT(PRODUCE_DATE, '%Y-%m') = '2022-05'
-# group by PRODUCT_ID;
