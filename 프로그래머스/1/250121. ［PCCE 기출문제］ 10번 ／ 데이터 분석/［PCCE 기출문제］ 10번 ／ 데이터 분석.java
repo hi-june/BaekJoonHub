00246ad3c57902ea37c01d9e239067ca796f2519
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-04-23 14:09:37 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 4.89 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -10,38 +10,14 @@ class Solution {
     );
     
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {        
-        List<List<Integer>> remainData = new ArrayList<>();
+        int extIdx = map.get(ext);
+        int sortIdx = map.get(sort_by);
         
-        for (int[] d : data) {
-            if (isAvail(d, ext, val_ext)) {
-                remainData.add(
-                    Arrays.stream(d)
-                    .boxed()
-                    .collect(Collectors.toList())
-                );
-            }
-        }
+        data = Arrays.stream(data)
+            .filter(d -> d[extIdx] < val_ext)
+            .sorted((d1, d2) -> d1[sortIdx] - d2[sortIdx])
+            .toArray(int[][]::new);
         
-        Collections.sort(remainData, (r1, r2) -> {
-            int idx = map.get(sort_by);
-            
-            return r1.get(idx) - r2.get(idx);
-        });
-        
-        int[][] answer = new int[remainData.size()][];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = remainData.get(i).stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
-        }
-        
-        return answer;
-    }
-    
-    private static boolean isAvail(int[] d, String ext, int val_ext) {
-        int idx = map.get(ext);
-        int value = d[idx];
-        
-        return value < val_ext;
+        return data;
     }
 }
