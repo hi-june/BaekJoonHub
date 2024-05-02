82a0c9ac2d1c4a1214797551362a2dc718f347ff
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-05-02 18:58:56 +0900

    [level 3] Title: N으로 표현, Time: 12.56 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -4,17 +4,16 @@ class Solution {
     public int solution(int N, int number) {
         Map<Integer, Set<Integer>> nums = new HashMap<>();
         
-        for (int i = 0; i < 9; i++) {
+        for (int i = 2; i < 9; i++) {
             nums.put(i, new HashSet<>());
         }
         
-        nums.get(1).add(N);
+        nums.put(1, Set.of(N));
         
         for (int i = 2; i < 9; i++) {
             Set<Integer> set = nums.get(i);
             
-            // 여러 경우의 수 조합
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j < i; j++) {
                 Set<Integer> preSet = nums.get(j);
                 Set<Integer> postSet = nums.get(i - j);
                 
@@ -23,22 +22,16 @@ class Solution {
                         set.add(preNum + postNum);
                         set.add(preNum - postNum);
                         set.add(preNum * postNum);
-                        
-                        if (postNum != 0) {
-                            set.add(preNum / postNum);
-                        }
+                        if (postNum != 0) set.add(preNum / postNum);
                     }
                 }
             }
             
-            // i개의 N으로 이루어진 숫자 추가
             set.add(Integer.valueOf(Integer.toString(N).repeat(i)));
         }
         
         for (int k = 1; k < 9; k++) {
-            Set<Integer> set = nums.get(k);
-            
-            if (set.contains(number)) return k;
+            if (nums.get(k).contains(number)) return k;
         }
         
         return -1;
