273e679cd06705d0cb7d4350386c2e75cc0eae44
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-05-02 17:27:52 +0900

    [level 3] Title: 등굣길, Time: 0.86 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -4,20 +4,25 @@ class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int[][] map = new int[n + 1][m + 1];
         
+        // 갈 수 없는 땅 표시
         for (int[] p : puddles) {
             map[p[1]][p[0]] = -1;
         }
         
+        // 시작 점 표시
         map[1][1] = 1;
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < m + 1; j++) {
-                if (map[i][j] == -1) continue;
+        
+        // 최단 경로 표시하기(n: 행, m: 열)
+        for (int col = 1; col < n + 1; col++) {
+            for (int row = 1; row < m + 1; row++) {
+                if (map[col][row] == -1) continue;
                 
-                if (map[i - 1][j] > 0) {
-                    map[i][j] += map[i - 1][j] % MOD;
+                if (map[col - 1][row] > 0) {    // 위쪽 칸
+                    map[col][row] += map[col - 1][row] % MOD;
                 }
-                if (map[i][j - 1] > 0) {
-                    map[i][j] += map[i][j - 1] % MOD;
+                
+                if (map[col][row - 1] > 0) {    // 왼쪽 칸
+                    map[col][row] += map[col][row - 1] % MOD;
                 }
             }
         }
