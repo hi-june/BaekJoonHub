1530af9c862de3374b208f61d2fe895c14a7d5b0
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-05-16 22:39:54 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.49 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,20 @@
+class Solution {
+    private long[] mem;
+    
+    public long solution(int n) {
+        if (n == 1) return 1;
+        
+        // an = a(n - 1) + a(n - 2)
+        mem = new long[n + 1];
+        mem[1] = 1;
+        mem[2] = 2;
+        
+        return jump(n);
+    }
+    
+    private long jump(int n) {
+        if (mem[n] != 0) return mem[n];
+        
+        return mem[n] = ((jump(n - 1) % 1234567) + (jump(n - 2) % 1234567)) % 1234567;
+    }
+}
