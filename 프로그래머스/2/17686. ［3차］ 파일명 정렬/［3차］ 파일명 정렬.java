3c3dd042044c54e35430b602a694f86438c2b57d
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-05-31 17:20:54 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 11.41 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -3,53 +3,23 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] files) {
         Arrays.sort(files, (f1, f2) -> {
+            // 문자열 중간에 연속되는 숫자 인덱스 범위 찾기
             int f1IdxS = 0;
-            for (int i = 0; i < f1.length(); i++) {
-                if (Character.isDigit(f1.charAt(i))) {
-                    f1IdxS = i;
-                    break;
-                }
-            }
-            int f1IdxE = f1.length();
-            for (int i = f1IdxS + 1; i < f1.length(); i++) {
-                if (!Character.isDigit(f1.charAt(i))) {
-                    f1IdxE = i;
-                    break;
-                }
-                
-                if (i == f1IdxS + 5) {
-                    f1IdxE = i;
-                    break;
-                }
-            }
-            
             int f2IdxS = 0;
-            for (int i = 0; i < f2.length(); i++) {
-                if (Character.isDigit(f2.charAt(i))) {
-                    f2IdxS = i;
-                    break;
-                }
-            }
-            int f2IdxE = f2.length();
-            for (int i = f2IdxS + 1; i < f2.length(); i++) {
-                if (!Character.isDigit(f2.charAt(i))) {
-                    f2IdxE = i;
-                    break;
-                }
-                
-                if (i == f2IdxS + 5) {
-                    f2IdxE = i;
-                    break;
-                }
-            }
+            while (f1IdxS < f1.length() && !Character.isDigit(f1.charAt(f1IdxS))) f1IdxS++;
+            while (f2IdxS < f2.length() && !Character.isDigit(f2.charAt(f2IdxS))) f2IdxS++;
             
+            int f1IdxE = f1IdxS;
+            int f2IdxE = f2IdxS;
+            while (f1IdxE < f1.length() && Character.isDigit(f1.charAt(f1IdxE))) f1IdxE++;
+            while (f2IdxE < f2.length() && Character.isDigit(f2.charAt(f2IdxE))) f2IdxE++;
+            
+            // 정렬을 위한 비교
             String f1Head = f1.substring(0, f1IdxS).toLowerCase();
             int f1Number = Integer.parseInt(f1.substring(f1IdxS, f1IdxE));
-            // String f1Tail = f1.substring(f1IdxE);
             
             String f2Head = f2.substring(0, f2IdxS).toLowerCase();
             int f2Number = Integer.parseInt(f2.substring(f2IdxS, f2IdxE));
-            // String f2Tail = f2.substring(f2IdxE);
             
             if (f1Head.compareTo(f2Head) == 0) {
                 return f1Number - f2Number;
