723ff0c5a3462fb04ece423991ef175e3bfb42e4
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-07-09 19:10:48 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 3.08 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,27 +1,18 @@
+// greedy
 class Solution {
-    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        int[] dRev = new int[deliveries.length];
-        for (int i = deliveries.length - 1; i >= 0; i--) {
-            dRev[deliveries.length - i - 1] = deliveries[i];
-        }
-        
-        int[] pRev = new int[pickups.length];
-        for (int i = pickups.length - 1; i >= 0; i--) {
-            pRev[pickups.length - i - 1] = pickups[i];
-        }
-        
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {        
         long answer = 0;
         
         int deli = 0;
         int pick = 0;
-        for (int i = 0; i < n; i++) {
-            deli += dRev[i];
-            pick += pRev[i];
+        for (int i = n - 1; i >= 0; i--) {
+            deli += deliveries[i];
+            pick += pickups[i];
             
             while (deli > 0 || pick > 0) {
                 deli -= cap;
                 pick -= cap;
-                answer += (n - i) * 2;
+                answer += (i + 1) * 2;
             }
         }
         
