750b64ced3305a9398f5333ebc0ce20ab8c21f4b
최준혁 <98803599+hi-june@users.noreply.github.com>
2024-07-16 23:11:10 +0900

    [level 2] Title: 요격 시스템, Time: 364.21 ms, Memory: 178 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -1,34 +1,31 @@
-import java.util.*;
+import java.util.Arrays;
 
-// 2024 현대 오토에버 기출과 유사
 class Solution {
+    // 2024 현대 오토에버 기출과 유사
+    // 그리디, 정렬
     public int solution(int[][] targets) {
-        // [[1,4], [3,7], [4,5], [4,8], [5,12], [10,14], [11,13]]
         Arrays.sort(targets, (t1, t2) -> t1[0] - t2[0]);
         
-        int answer = 0;        
-        int preStart = targets[0][0];   // 1
-        int preEnd = targets[0][1]; // 4
-
-        for (int[] target : targets) {
-            if (answer == 0) {
-                answer += 1;
-                continue;
-            }
+        int answer = 1;
+        int preS = targets[0][0];
+        int preE = targets[0][1];
+        
+        for (int i = 1; i < targets.length; i++) {
+            int[] target = targets[i];
             
-            int curStart = target[0];
-            int curEnd = target[1];
+            int curS = target[0];
+            int curE = target[1];
             
-            if (preStart <= curStart && curStart < preEnd) {
-                preStart = Math.max(preStart, curStart);
-                preEnd = Math.min(preEnd, curEnd);
+            if (curS >= preS && curS < preE) {
+                preS = Math.max(preS, curS);
+                preE = Math.min(preE, curE);
             } else {
-                preStart = curStart;
-                preEnd = curEnd;
-                answer += 1;
+                preS = curS;
+                preE = curE;
+                answer++;
             }
         }
-    
+        
         return answer;
     }
 }
