8b9ed29c41124acc55b76e9e1db0b1eb3a221ccf
최준혁 <98803599+hi-june@users.noreply.github.com>
2025-09-24 22:07:04 +0900

    [Silver I] Title: 단지번호붙이기, Time: 136 ms, Memory: 16448 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,12 +1,14 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.*;
 
 public class Main {
-    private static final int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};  // col, row
-    private static int[][] map;
-    private static int N;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    private static class Node {
+    static int[][] map;
+    static int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static class Node {
         int col;
         int row;
 
@@ -17,38 +19,53 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        // Main main = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         map = new int[N][N];
 
-        // map 초기화
         for (int i = 0; i < N; i++) {
-            int[] cols = Arrays.stream(br.readLine().split(""))
+            map[i] = Arrays.stream(br.readLine().split(""))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-
-            map[i] = cols;
         }
 
-        int count = 0;
-        List<Integer> sizes = new ArrayList<>();
-        for (int i = 0; i < N; i++) {   // col
-            for (int j = 0; j < N; j++) {   // row
-                if (map[i][j] != 0) {
-                    int size = visitAll(i, j);
-                    sizes.add(size);
-                    count++;
+        List<Integer> houseCount = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 0) continue;
+
+                int count = 0;
+
+                Queue<Node> queue = new LinkedList<>();
+                queue.offer(new Node(i, j));
+                map[i][j] = 0;
+                count++;
+
+                while (!queue.isEmpty()) {
+                    Node node = queue.poll();
+
+                    for (int[] d : directions) {
+                        int dCol = node.col + d[0];
+                        int dRow = node.row + d[1];
+
+                        if (dCol >= N || dCol < 0 || dRow >= N || dRow < 0) continue;
+
+                        if (map[dCol][dRow] != 0) {
+                            queue.offer(new Node(dCol, dRow));
+                            map[dCol][dRow] = 0;
+                            count++;
+                        }
+                    }
                 }
+
+                if (count > 0) houseCount.add(count);
             }
         }
-        Collections.sort(sizes);
 
-        bw.write(count + "\n");
-        for (int s : sizes) {
-            bw.write(s + "\n");
+        Collections.sort(houseCount);
+        bw.write(houseCount.size() + "\n");
+
+        for (int c : houseCount) {
+            bw.write(c + "\n");
         }
 
         bw.flush();
@@ -56,32 +73,6 @@ public class Main {
         br.close();
         bw.close();
     }
-
-    private static int visitAll(int col, int row) {
-        int size = 0;
-
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(col, row));
-        map[col][row] = 0;
-        size++;
-
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-
-            for (int[] direction : directions) {
-                int newCol = node.col + direction[0];
-                int newRow = node.row + direction[1];
-
-                if (newCol >= N || newCol < 0 || newRow >= N || newRow < 0) continue;
-
-                if (map[newCol][newRow] == 0) continue;
-
-                queue.offer(new Node(newCol, newRow));
-                map[newCol][newRow] = 0;
-                size++;
-            }
-        }
-
-        return size;
-    }
 }
+
+
