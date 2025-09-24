1c279352d955a68a0b514bc48cde577d5894ff55
최준혁 <98803599+hi-june@users.noreply.github.com>
2025-09-24 23:01:04 +0900

    [Silver II] Title: 촌수계산, Time: 116 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,52 +1,63 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.*;
 
 public class Main {
-    public static int answer = -1;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int target1 = Integer.parseInt(st.nextToken());
-        int target2 = Integer.parseInt(st.nextToken());
-
-        int relation = Integer.parseInt(br.readLine());
-
+        int N = Integer.parseInt(br.readLine());
         List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < n + 1; i++) {
+        for (int i = 0; i < N + 1; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < relation; i++) {
-            st = new StringTokenizer(br.readLine());
+        int[] inputs = Arrays.stream(br.readLine().split(" "))
+            .mapToInt(Integer::parseInt)
+            .toArray();
+        int t1 = inputs[0];
+        int t2 = inputs[1];
 
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(br.readLine());
+        for (int i = 0; i < E; i++) {
+            inputs = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-            graph.get(x).add(y);
-            graph.get(y).add(x);
+            int f = inputs[0];
+            int t = inputs[1];
+
+            graph.get(f).add(t);
+            graph.get(t).add(f);
         }
 
-        boolean[] visited = new boolean[n + 1];
+        // t1 -> t2
+        int[] dists = new int[N + 1];
+        Arrays.fill(dists, -1);
 
-        dfs(graph, visited, target1, target2, 0);
-        System.out.println(answer);
-    }
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(t1);
+        dists[t1] = 0;
 
-    public static void dfs(List<List<Integer>> graph, boolean[] visited, int target1, int target2, int depth) {
-        visited[target1] = true;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            if (node == t2) break;
 
-        for (int node : graph.get(target1)) {
-            if (!visited[node]) {
-                if (node == target2) {
-                    answer = depth + 1;
-                    return;
+            for (int adj : graph.get(node)) {
+                if (dists[adj] == -1) {
+                    queue.offer(adj);
+                    dists[adj] = dists[node] + 1;
                 }
-                dfs(graph, visited, node, target2, depth + 1);
             }
         }
+
+        bw.write(dists[t2] + "");
+        bw.flush();
+
+        br.close();
+        bw.close();
     }
 }
+
+
